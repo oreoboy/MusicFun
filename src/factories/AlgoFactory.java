a4cbf5a847fba682e11704ae5b2a2c4a7b1637cc
oreoboy <oreoboy@live.fr>
2013-12-18 14:04:49 +0100

    Mettre une hashmap

--- src/factories/AlgoFactory.java
+++ src/factories/AlgoFactory.java
@@ -1,20 +1,33 @@
 package factories;
 
-import creationAlgo.Aleatoire;
+import java.util.HashMap;
+import java.util.Map;
+
 import creationAlgo.Algo;
-import creationAlgo.Arpege;
 
 public class AlgoFactory {
+	
+	private static Map<String, String> mapAlgo = new HashMap<String, String>();
+	
+	static {
+		mapAlgo.put("Aléatoire", "Aleatoire");
+		mapAlgo.put("Arpege", "Arpege");
+	}
 
 	public Algo getAlgo(String typeAlgo) throws Exception {
 		Algo algo;
-		if ("Aleatoire".equals(typeAlgo)) {
-			algo = new Aleatoire();
-		} else if ("Arpege".equals(typeAlgo)) {
-			algo = new Arpege();
-		} else {
-			throw new Exception("Impossible de créer un " + typeAlgo);
-		}
+//		if ("Aleatoire".equals(typeAlgo)) {
+//			algo = new Aleatoire();
+//		} else if ("Arpege".equals(typeAlgo)) {
+//			algo = new Arpege();
+//		} else {
+//			throw new Exception("Impossible de créer un " + typeAlgo);
+//		}
+		
+		algo = (Algo) Class.forName(mapAlgo.get(typeAlgo)).newInstance();
+		
+		
+		
 		return algo;
 	}
 
